e3b726c54ce19241f8f04abb7181535f0cd85943
Santiago Ferreira <santiago@orientme.com.br>
2025-05-16 23:05:58 -0300

    dolar

--- src/app/components/taxa/Taxa.tsx
+++ src/app/components/taxa/Taxa.tsx
@@ -8,19 +8,28 @@ import { Label } from "@/components/ui/label";
 
 export default function InterestCalculator() {
   const [valorInicial, setValorInicial] = useState(0);
-  const [taxa, setTaxa] = useState(0); // em porcentagem
+  const [taxa, setTaxa] = useState(0);
   const [dias, setDias] = useState(0);
   const [intervalo, setIntervalo] = useState(0);
   const [cotacao, setCotacao] = useState<number | null>(null);
-  const [resultado, setResultado] = useState<number | null>(0);
-  const [dolar, setDolar] = useState(0);
+  const [resultado, setResultado] = useState<number | null>(null);
   const [loading, setLoading] = useState(false);
+  const [erro, setErro] = useState(false);
 
   const calcular = () => {
     const ciclos = dias / intervalo;
     const taxaDecimal = taxa / 100;
     const valorFinal = valorInicial * Math.pow(1 + taxaDecimal, ciclos);
     setResultado(Number(valorFinal.toFixed(2)));
+  };
+
+  const clearAll = () => {
+    setValorInicial(0);
+    setTaxa(0);
+    setDias(0);
+    setIntervalo(0);
+    setResultado(null);
+    setErro(false);
   };
 
   useEffect(() => {
@@ -40,7 +49,7 @@ export default function InterestCalculator() {
     }
     fetchDolar();
 
-    const intervaloId = setInterval(fetchDolar, 5 * 3600000);
+    const intervaloId = setInterval(fetchDolar, 3 * 3600);
     return () => clearInterval(intervaloId);
   }, []);
 
@@ -48,10 +57,14 @@ export default function InterestCalculator() {
   return (
     <Card className="w-full max-w-md p-6 shadow-xl border border-gray-300 rounded-3xl bg-gradient-to-br ">
       <CardContent className="space-y-6">
+        <div className="text-right flex justify-end gap-1">
+          <h3>US$:</h3>
+          <p>R${cotacao?.toFixed(2).replace(".", ",")} 💵 </p>
+        </div>
         <h2 className="text-2xl font-extrabold text-center text-slate-800 tracking-tight">
           💹 Simulador de Juros Compostos
         </h2>
-
+        {erro && <p>Por favor, preencha todos os campos</p>}
         <div className="grid gap-4">
           <div>
             <Label htmlFor="valorInicial" className="text-sm text-slate-600">
@@ -60,7 +73,7 @@ export default function InterestCalculator() {
             <Input
               id="valorInicial"
               type="number"
-              value={valorInicial}
+              value={valorInicial > 0 ? valorInicial : ""}
               onChange={(e) => setValorInicial(Number(e.target.value))}
               className="mt-1"
             />
@@ -73,7 +86,7 @@ export default function InterestCalculator() {
             <Input
               id="taxa"
               type="number"
-              value={taxa}
+              value={taxa > 0 ? taxa : ""}
               onChange={(e) => setTaxa(Number(e.target.value))}
               className="mt-1"
             />
@@ -86,7 +99,7 @@ export default function InterestCalculator() {
             <Input
               id="dias"
               type="number"
-              value={dias}
+              value={dias > 0 ? dias : ""}
               onChange={(e) => setDias(Number(e.target.value))}
               className="mt-1"
             />
@@ -99,19 +112,30 @@ export default function InterestCalculator() {
             <Input
               id="intervalo"
               type="number"
-              value={intervalo}
+              value={intervalo > 0 ? intervalo : ""}
               onChange={(e) => setIntervalo(Number(e.target.value))}
               className="mt-1"
             />
           </div>
         </div>
+        <div className="flex gap-5">
+          <Button
+            onClick={
+              intervalo > 0
+                ? calcular
+                : () => {
+                    setErro(true);
+                  }
+            }
+            className=" bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold hover:brightness-110 transition"
+          >
+            Calcular
+          </Button>
 
-        <Button
-          onClick={calcular}
-          className="w-full bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold hover:brightness-110 transition"
-        >
-          Calcular
-        </Button>
+          <Button onClick={clearAll} variant={"destructive"}>
+            Limpar
+          </Button>
+        </div>
 
         {resultado !== null && (
           <>
